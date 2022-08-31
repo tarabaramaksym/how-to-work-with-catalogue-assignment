697ebc13abd9713e1fb89c7bc7a90ebd502ce913
Maksym Tarabara <maksym.tarabara@scandiweb.com>
2022-08-31 10:41:58 +0300

    Addressed all the issues with the Scandiweb_Test module, docs, formatting, quantity

--- app/code/Scandiweb/Test/Setup/Patch/Data/CreateTestSimpleProduct.php
+++ app/code/Scandiweb/Test/Setup/Patch/Data/CreateTestSimpleProduct.php
@@ -19,43 +19,93 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Validation\ValidationException;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Framework\Setup\Patch\PatchInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+
 
 class CreateTestSimpleProduct implements DataPatchInterface
 {
 
+    /**
+     * @var ProductInterfaceFactory
+     */
     protected ProductInterfaceFactory $productInterfaceFactory;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
     protected ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var State
+     */
     protected State $appState;
+
+    /**
+     * @var EavSetup
+     */
     protected EavSetup $eavSetup;
+
+    /**
+     * @var CategoryLinkManagementInterface
+     */
     protected CategoryLinkManagementInterface $categoryLink;
 
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
+     * @var array
+     */
+    protected array $sourceItems = [];
+
+    /**
+     * @param ProductInterfaceFactory $productInterfaceFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param State $appState
+     * @param EavSetup $eavSetup
+     * @param CategoryLinkManagementInterface $categoryLink
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     */
     public function __construct(
         ProductInterfaceFactory         $productInterfaceFactory,
         ProductRepositoryInterface      $productRepository,
         State                           $appState,
         EavSetup                        $eavSetup,
-        CategoryLinkManagementInterface $categoryLink
-    )
-    {
-        $this->appState = $appState;
+        CategoryLinkManagementInterface $categoryLink,
+        SourceItemInterfaceFactory      $sourceItemFactory,
+        SourceItemsSaveInterface        $sourceItemsSaveInterface
+    ) {
         $this->productInterfaceFactory = $productInterfaceFactory;
         $this->productRepository = $productRepository;
+        $this->appState = $appState;
         $this->eavSetup = $eavSetup;
         $this->categoryLink = $categoryLink;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
     }
 
     /**
      * Add new product
+     * @return void
      * @throws \Exception
      */
-    public function apply(): PatchInterface
+    public function apply(): void
     {
         $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
-        return $this;
     }
 
     /**
+     * @return void
      * @throws CouldNotSaveException
      * @throws InputException
      * @throws LocalizedException
@@ -66,12 +116,12 @@ class CreateTestSimpleProduct implements DataPatchInterface
     {
         try {
             // if element is found nothing happens
-            // if it is not found, throws exception and adds product
-            // wasn't able to find a better way to do it
+            // otherwise, throws exception and adds product
             $this->productRepository->get('simple-test-product');
         } catch (NoSuchEntityException) {
             $product = $this->productInterfaceFactory->create();
             $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+
             $product->setTypeId(Type::TYPE_SIMPLE)
                 ->setAttributeSetId($attributeSetId)
                 ->setName('Simple Test Product')
@@ -79,8 +129,16 @@ class CreateTestSimpleProduct implements DataPatchInterface
                 ->setPrice(12.11)
                 ->setVisibility(Visibility::VISIBILITY_BOTH)
                 ->setStatus(Status::STATUS_ENABLED);
-
             $product = $this->productRepository->save($product);
+
+            $sourceItem = $this->sourceItemFactory->create();
+            $sourceItem->setSourceCode('default');
+            $sourceItem->setQuantity(250);
+            $sourceItem->setSku($product->getSku());
+            $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+            $this->sourceItems[] = $sourceItem;
+
+            $this->sourceItemsSaveInterface->execute($this->sourceItems);
 
             $this->categoryLink->assignProductToCategories($product->getSku(), [2]);
         }
@@ -88,6 +146,7 @@ class CreateTestSimpleProduct implements DataPatchInterface
 
     /**
      * {@inheritDoc}
+     * @return array|string[]
      */
     public static function getDependencies(): array
     {
@@ -96,6 +155,7 @@ class CreateTestSimpleProduct implements DataPatchInterface
 
     /**
      * {@inheritDoc}
+     * @return array|string[]
      */
     public function getAliases(): array
     {
